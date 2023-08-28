6659ab221305a7d64a4ca56e13975fd483869118
Mike Koval <mika.smith@gmail.com>
2023-08-28 16:40:33 -0400

    add transformer remark

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,7 @@ module.exports = {
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
+    `gatsby-transformer-remark`,
     "gatsby-plugin-postcss",
     "gatsby-plugin-sitemap",
     {
